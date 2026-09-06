36655ac39e530e6b892146047005659238136cc3
Nikita Plotnikov <pnik10969@gmail.com>
2026-09-06 20:16:38 +0300

    add auction service process func

--- internal/auction/service.go
+++ internal/auction/service.go
@@ -2,6 +2,7 @@ package auction
 
 import (
 	"context"
+	"fmt"
 	"mini-adex/internal/domain"
 )
 
@@ -17,4 +18,19 @@ func NewService(partners PartnerRepository) *Service {
 	return &Service{
 		partners: partners,
 	}
+}
+
+func (s *Service) Process(
+	ctx context.Context,
+	req domain.AuctionRequest,
+) (domain.AuctionResult, error) {
+	partners, err := s.partners.List(ctx)
+
+	if err != nil {
+		return domain.AuctionResult{}, fmt.Errorf("list partners: %w", err)
+	}
+
+	matched := Filter(req, partners)
+
+	return domain.AuctionResult{}, nil
 }

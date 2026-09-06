1f94d2fedc43be1f3ac6dd91328b3d35e92fe460
Nikita Plotnikov <pnik10969@gmail.com>
2026-09-06 20:11:16 +0300

    add postgres repos

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"mini-adex/internal/auction"
+	"mini-adex/repository/postgres"
 
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	ctx := context.Background()
+
+	db, err := pgxpool.New(
+		ctx,
+		"postgres://mini_adex:mini_adex@127.0.0.1:5433/mini_adex?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatalf("create postgres pool: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(ctx); err != nil {
+		log.Fatalf("ping postgres: %v", err)
+	}
+
+	partnerRepo := postgres.NewPartnerRepository(db)
+
+	service := auction.NewService(partnerRepo)
+
+	partners, err := partnerRepo.List(ctx)
 }
--- repository/postgres/partner.go
+++ repository/postgres/partner.go
@@ -1,1 +1,69 @@
 package postgres
+
+import (
+	"context"
+	"fmt"
+	"mini-adex/internal/domain"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PartnerRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewPartnerRepository(db *pgxpool.Pool) *PartnerRepository {
+	return &PartnerRepository{
+		db: db,
+	}
+}
+
+func (r *PartnerRepository) List(
+	ctx context.Context,
+) ([]domain.Partner, error) {
+	const query = `
+		SELECT 
+			uid,
+			name,
+			endpoint,
+			is_enabled,
+			countries,
+			device_types,
+			min_bid_floor,
+			blocked_categories
+			FROM partners
+	`
+
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("query partners: %w", err)
+	}
+	defer rows.Close()
+
+	partners := make([]domain.Partner, 0)
+
+	for rows.Next() {
+		var partner domain.Partner
+
+		if err := rows.Scan(
+			&partner.UID,
+			&partner.Name,
+			&partner.Endpoint,
+			&partner.IsEnabled,
+			&partner.Countries,
+			&partner.DeviceTypes,
+			&partner.MinBidFloor,
+			&partner.BlockedCategories,
+		); err != nil {
+			return nil, fmt.Errorf("scan partner: %w", err)
+		}
+
+		partners = append(partners, partner)
+	}
+	
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("iterate partners: %w", err)
+	}
+
+	return partners, nil
+}

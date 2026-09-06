16b59a5dd810c14d5f11d24b87ffb82a70b6aaef
Nikita Plotnikov <pnik10969@gmail.com>
2026-09-06 21:12:58 +0300

    add parallel DSP requests

--- internal/auction/service.go
+++ internal/auction/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"mini-adex/internal/domain"
+	"sync"
 	"time"
 )
 
@@ -16,20 +17,20 @@ type DSPClient interface {
 		ctx context.Context,
 		partner domain.Partner,
 		req domain.AuctionRequest,
-	)
+	) error
 }
 
 type Service struct {
 	partners PartnerRepository
 	dsp      DSPClient
-	timeout time.Duration
+	timeout  time.Duration
 }
 
 func NewService(
 	partners PartnerRepository,
 	dsp DSPClient,
 	timeout time.Duration,
-	) *Service {
+) *Service {
 	return &Service{
 		partners: partners,
 	}
@@ -39,6 +40,8 @@ func (s *Service) Process(
 	ctx context.Context,
 	req domain.AuctionRequest,
 ) (domain.AuctionResult, error) {
+	started := time.Now()
+
 	partners, err := s.partners.List(ctx)
 
 	if err != nil {
@@ -46,8 +49,61 @@ func (s *Service) Process(
 	}
 
 	matched := Filter(req, partners)
-	
+	successful := s.sendToPartners(ctx, req, partners)
 
+	return domain.AuctionResult{
+		RequestID:   req.RequestID,
+		MatchedDSPs: partnerUIDs(matched),
+		Sent:        len(matched),
+		Succeeded:   successful,
+		DurationMS:  int64(time.Since(started).Milliseconds()),
+	}, nil
+}
 
-	return domain.AuctionResult{}, nil
+func (s *Service) sendToPartners(
+	ctx context.Context,
+	req domain.AuctionRequest,
+	partners []domain.Partner,
+) int {
+	auctionCtx, cancel := context.WithTimeout(ctx, s.timeout)
+
+	defer cancel()
+
+	var wg sync.WaitGroup
+
+	resultsChar := make(chan error, len(partners))
+
+	for _, partner := range partners {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			resultsChar <- s.dsp.Bid(auctionCtx, partner, req)
+
+		}()
+	}
+
+	wg.Wait()
+	close(resultsChar)
+
+	successful := 0
+
+	for err := range resultsChar {
+		if err == nil {
+			successful++
+		}
+	}
+
+	return successful
+}
+
+func partnerUIDs(partners []domain.Partner) []string {
+	uids := make([]string, 0, len(partners))
+
+	for _, partner := range partners {
+		uids = append(uids, partner.UID)
+	}
+
+	return uids
 }
